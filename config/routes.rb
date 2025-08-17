c80c54bd60fd46654d1e396937f08a09b3f0b3ca
NotFabianML <fabianmloaiza@gmail.com>
2025-08-16 22:29:33 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  #  El scope (:locale) hará que tus URLs tengan /es o /en
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
     resources :posts
     root "home#index"
-    # get "home/index"
     get "/dashboard", to: "dashboard#index"
     get "/browse", to: "browse#index"
 
     resources :users, only: [ :index, :show ]
-
     resource :profile, only: [ :show, :edit, :update ]
+    resources :sessions, only: [ :index, :new, :create, :show ]
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
