9be4b27a2370a19552543f41aff323b28a58f68e
NotFabianML <fabianmloaiza@gmail.com>
2025-08-17 20:35:34 -0600

    Update index.ts

--- app/javascript/types/index.ts
+++ app/javascript/types/index.ts
@@ -1,11 +1,13 @@
 // Este archivo actúa como el punto de entrada para todos nuestros tipos.
 
 export * from './home.types';
-export * from './posts.types';
 export * from './devise.types';
 export * from './profiles.types';
 export * from './users.types';
 export * from './sessions.types';
+export * from './dashboard.types';
+export * from './browse.types';
+export * from './discover_sessions.types';
 
 export interface LocaleData {
   en_url: string;
@@ -15,7 +17,15 @@ export interface LocaleData {
 
 export interface UserData {
   logged_in: boolean;
+  id: number | null;
   email: string | null;
+  first_name: string | null;
+  last_name: string | null;
+  abilities: {
+    can_create_session: boolean;
+    can_view_leaderboard: boolean;
+    // ... otros permisos
+  };
 }
 
 export interface NavbarI18n {
@@ -24,6 +34,10 @@ export interface NavbarI18n {
   log_out: string;
   log_in: string;
   sign_up: string;
+  chat_link: string;
+  leaderboard_link: string;
+  profile_link: string;
+  sessions_link: string;
 }
 
 export interface SharedProps {
