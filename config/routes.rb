4fa74cf81ecc982ef6fc8e054c120f9601433443
NotFabianML <fabianmloaiza@gmail.com>
2025-08-17 20:37:32 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
-    resources :posts
     root "home#index"
     get "/dashboard", to: "dashboard#index"
     get "/browse", to: "browse#index"
 
     resources :users, only: [ :index, :show ]
-    resource :profile, only: [ :show, :edit, :update ]
-    resources :sessions, only: [ :index, :new, :create, :show ]
+    resource :profile, only: [ :show, :edit, :update, :destroy ]
+    resources :sessions, only: [ :index, :new, :create, :show ] do
+      member do
+        post :join
+        patch :notepad
+      end
+    end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
