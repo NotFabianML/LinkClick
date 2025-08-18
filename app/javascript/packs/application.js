c6976384fdaa3419ff915f1573792ccd6166ccea
NotFabianML <fabianmloaiza@gmail.com>
2025-08-17 20:35:38 -0600

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,13 +13,13 @@ import ForgotPasswordComponent from "../pages/devise/ForgotPassword";
 // 2. Importa tus componentes originales
 import NavbarComponent from "../components/layout/Navbar";
 import HomeComponent from "../pages/Home";
-import PostsViewerComponent from "../pages/PostsViewer";
 import DashboardComponent from "../pages/Dashboard"
 import BrowseComponent from "../pages/Browse"
 import MyProfileComponent from "../pages/MyProfile";
 import UserProfileComponent from "../pages/UserProfile";
 import CreateSessionComponent from "../pages/CreateSession";
 import SessionDetailComponent from "../pages/SessionDetail";
+import DiscoverSessionsPage from "../pages/DiscoverSessions";
 
 
 // 3. Envuelve cada componente que necesite los providers
@@ -28,13 +28,13 @@ const SignUp = withProviders(SignUpComponent);
 const ForgotPassword = withProviders(ForgotPasswordComponent);
 const Navbar = withProviders(NavbarComponent);
 const Home = withProviders(HomeComponent);
-const PostsViewer = withProviders(PostsViewerComponent);
 const Dashboard = withProviders(DashboardComponent);
 const Browse = withProviders(BrowseComponent);
 const MyProfile = withProviders(MyProfileComponent);
 const UserProfile = withProviders(UserProfileComponent);
 const CreateSession = withProviders(CreateSessionComponent);
 const SessionDetail = withProviders(SessionDetailComponent);
+const DiscoverSessions = withProviders(DiscoverSessionsPage);
 
 // 4. Registra las versiones finales "envueltas"
 ReactOnRails.register({
@@ -43,14 +43,16 @@ ReactOnRails.register({
   ForgotPassword,
   Navbar,
   Home,
-  PostsViewer,
   Dashboard,
   Browse,
   MyProfile,
   UserProfile,
   CreateSession,
-  SessionDetail
+  SessionDetail,
+  DiscoverSessions,
 });
+
+
 
 document.addEventListener("turbo:load", () => {
   ReactOnRails.reactOnRailsPageLoaded();
