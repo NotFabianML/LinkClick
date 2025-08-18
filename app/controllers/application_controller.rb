7db4c7ed7375be0b4f452a9bcb5435c49b5f2e1f
NotFabianML <fabianmloaiza@gmail.com>
2025-08-17 20:34:35 -0600

    feat: Add ability.rb for CanCanCan Guard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,37 +7,65 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    dashboard_path(locale: I18n.locale)
+  end
+
+  def after_sign_up_path_for(resource)
+    new_user_session_path(locale: I18n.locale)
+  end
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :role ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name, :bio, :linkedin_url, :github_url ])
   end
 
+  private
+
   def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
+    locale = params[:locale] || session[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+    session[:locale] = locale
   end
 
   def default_url_options
     { locale: I18n.locale }
   end
 
-  # Este método prepara los datos que estarán disponibles en todas las páginas
   def set_shared_props
+    # path = request.path
+
     @shared_props = {
       i18n: {
         navbar: t("navbar")
       },
       user: {
         logged_in: user_signed_in?,
-        email: user_signed_in? ? current_user.email : nil
+        id: user_signed_in? ? current_user.id : nil,
+        email: user_signed_in? ? current_user.email : nil,
+        first_name: user_signed_in? ? current_user.first_name : nil,
+        last_name: user_signed_in? ? current_user.last_name : nil,
+
+        abilities: {
+          can_create_session: user_signed_in? && can?(:create, Session),
+          can_view_leaderboard: user_signed_in? && can?(:view, :leaderboard) # Ejemplo para el futuro
+          # Añade aquí cualquier otro permiso que el frontend necesite conocer.
+        }
       },
       locale_data: {
         current_locale: I18n.locale,
         # Genera URLs para la página actual en ambos idiomas
         en_url: url_for(params.to_unsafe_h.merge(locale: :en)),
         es_url: url_for(params.to_unsafe_h.merge(locale: :es))
+        # en_url: url_for(path: path, locale: :en),
+        # es_url: url_for(path: path, locale: :es)
       }
     }
   end
---
+++ app/models/ability.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the user here.
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, published: true
+    #
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+
+    # Los usuarios no autenticados (invitados) no tienen un objeto `user`.
+    user ||= User.new # guest user (not logged in)
+
+    # --- REGLAS GLOBALES PARA TODOS ---
+    # Todos pueden ver las sesiones públicas.
+    can :read, Session, is_public: true, status: :published
+
+    # --- REGLAS ESPECÍFICAS POR ROL ---
+    if user.admin?
+      # Los administradores pueden gestionar absolutamente todo.
+      can :manage, :all
+    elsif user.teacher?
+      # Los profesores pueden leer todas las sesiones públicas.
+      can :read, Session, status: :published
+
+      # Pueden crear nuevas sesiones.
+      can :create, Session
+
+      # Solo pueden actualizar o destruir las sesiones que ellos crearon.
+      can :update, Session, creator_id: user.id
+      can :destroy, Session, creator_id: user.id
+
+      # Pueden gestionar las solicitudes para unirse a sus propias sesiones.
+      can :manage, JoinRequest, session: { creator_id: user.id }
+
+    elsif user.student?
+      # Los estudiantes pueden leer todas las sesiones públicas.
+      can :read, Session, status: :published
+
+      # Pueden solicitar unirse a las sesiones.
+      can :join, Session
+
+      # pueden crear, actualizar o destruir sesiones.
+      can :create, Session
+      can :update, Session
+      can :destroy, Session
+    end
+  end
+end
